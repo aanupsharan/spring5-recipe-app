6b72028fa5881956ec20ae31a4f72809405401da
aanupsharan <anup.ambula@gmail.com>
2021-07-17 19:15:43 -0500

    added the Unit of measure with one-to-one relationship

--- src/main/java/guru/springframework/domain/Ingredient.java
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -10,8 +10,9 @@ public class Ingredient {
     private Long id;
     private String description;
     private BigDecimal amount;
-    //TODO: future
-    //private UnitOfMeasure uom;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     @ManyToOne
     private Recipe recipe;
@@ -47,4 +48,8 @@ public class Ingredient {
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
     }
+
+    public UnitOfMeasure getUom() { return uom; }
+
+    public void setUom(UnitOfMeasure uom) { this.uom = uom; }
 }
